559bb70b72bc2b212706f6c585efb9ab4662b9b5
Marcosoff <117524575+Marcosoff@users.noreply.github.com>
2024-03-19 17:20:44 -0300

    DIRF_2024.py

--- DIRF_2024.py
+++ DIRF_2024.py
@@ -24,7 +24,7 @@ st.write(
     "Este App emite o comprovante de rendimentos referente ao recebimento de até 1 precatório no ano de 2024. Caso tenha recebido mais de um pagamento, por favor entrar em contato com a Assessoria de Gestão de Precatórios. Fone: (98) 2055-2426."
 , style="text-align: justjy")
 
-left, right = st.columns(3)
+left, right = st.columns(2)
 
 
 
